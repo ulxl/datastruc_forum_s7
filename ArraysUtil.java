67a44460144901a86b3b8c64ef002aa5a6a12426
Lalu Hilmy Abdurrahman <laluhilmynet@gmail.com>
2025-03-16 06:51:40 +0700

    fix comment and add outpus images

--- ArraysUtil.java
+++ ArraysUtil.java
@@ -2,17 +2,17 @@ import java.util.Arrays;
 
 public class ArraysUtil {
     public static void main(String[] args) {
-        // Deklarasi array
+        // deklarasi array
         int[] numbers = {4, 1, 3, 2};
         
-        // Tampilkan array awal
+        // print array awal
         System.out.println("Awal: " + Arrays.toString(numbers));
         
-        // Urutkan array
+        // urutkan array
         Arrays.sort(numbers);
         System.out.println("Terurut: " + Arrays.toString(numbers));
         
-        // Isi array baru
+        // isi array baru
         int[] newArray = new int[3];
         Arrays.fill(newArray, 5);
         System.out.println("Diisi 5: " + Arrays.toString(newArray));
